343cf4ebd356eea0a25198113ae1ea22fe83ac49
The-ARJ <joshiaayush871@gmail.com>
2023-07-04 17:45:23 +0545

    user authentication implemented

---
+++ middleware/auth.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+
+const verifyUser = (req, res, next) => {
+    let authHeader = req.headers.authorization
+    if (!authHeader) {
+        res.status(401)
+        return next(new Error('No authentication information provided'))
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) return next(err)
+        req.user = decoded
+        console.log(req.user)
+        next()
+    })
+}
+
+const verifyAdmin = (req, res, next) => {
+    if (req.user && req.user.role === 'admin') {
+        next();
+    } else {
+        res.status(403);
+        return next(new Error('Not admin'));
+    }
+};
+
+const verifySuperAdmin = (req, res, next) => {
+    if (req.user && (req.user.role === 'admin' || req.user.role === 'superadmin')) {
+        next();
+    } else {
+        res.status(403);
+        return next(new Error('Not super admin'));
+    }
+};
+
+module.exports = { verifyUser, verifyAdmin, verifySuperAdmin }
